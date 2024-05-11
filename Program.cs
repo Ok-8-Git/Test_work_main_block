7e9eb83dfb7b05a2bf1748ac648ca802dc60ab28
Oksana Grodetskaya <grodeczkaya94@mail.ru>
2024-05-11 22:56:10 +0300

    added PrintArray function

--- Program.cs
+++ Program.cs
@@ -35,4 +35,21 @@ string[] CreateArrayElementLengthLessOrEqualThree(string[] array, int countLengt
     return newArray;
 }
 
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"\"{array[i]}\", ");
+        }
+        else
+        {
+            Console.Write($"\"{array[i]}\"");
+        }
+    }
+    Console.Write("]");
+}
+
 
